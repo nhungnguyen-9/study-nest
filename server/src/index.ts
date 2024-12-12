43f4f6606f6279b4f6164a39392624397f1a022d
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-12-12 12:26:53 +0700

    add landing page

---
+++ server/src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import * as dynamoose from 'dynamoose'
+import { log } from 'console'
+
+// route imports
+
+
+// config
+dotenv.config()
+const isProd = process.env.NODE_ENV === "production"
+
+if (!isProd) {
+    dynamoose.aws.ddb.local()
+}
+
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+// routes
+app.get("/", (req, res) => {
+    res.send('hello Orm')
+})
+
+// server
+const port = process.env.PORT || 3000
+if (!isProd) {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}
---
+++ server/src/models/transactionModel.ts
@@ -0,0 +1,42 @@
+import { model, Schema } from "dynamoose";
+
+
+const transactionSchema = new Schema({
+    userId: {
+        type: String,
+        hashKey: true,
+        required: true
+    },
+    transactionId: {
+        type: String,
+        rangeKey: true,
+        required: true
+    },
+    dateTime: {
+        type: String,
+        required: true
+    },
+    courseId: {
+        type: String,
+        required: true,
+        index: {
+            name: "CourseTransactionsIndex",
+            type: "global"
+        }
+    },
+    paymentProvider: {
+        type: String,
+        enum: ["vnpay", "vietqr"],
+        required: true
+    },
+    amount: Number,
+},
+    {
+        saveUnknown: true,
+        timestamps: true
+    }
+)
+
+const Transaction = model("Transaction", transactionSchema)
+
+export default Transaction
